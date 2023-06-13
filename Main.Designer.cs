a1f90b7a8062f7836454528cc39640d65f9c59fe
guarneh <tiago.guarnerio@davinci.edu.ar>
2023-06-13 16:21:47 -0300

    saque un dispose de una form que no iba y funciona todo

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -17,7 +17,7 @@
             {
                 components.Dispose();
             }
-            cinema.cerrar();
+            
             base.Dispose(disposing);
         }
 
--- MyContext.cs
+++ MyContext.cs
@@ -25,9 +25,11 @@ namespace Cinemania
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //Conexion Casa
-            optionsBuilder.UseSqlServer("Data Source=DESKTOP-NH6VC1C\\SQLEXPRESS;Initial Catalog=CineDotNetV2;Integrated Security=True ; Encrypt=false");
+            //optionsBuilder.UseSqlServer("Data Source=DESKTOP-NH6VC1C\\SQLEXPRESS;Initial Catalog=CineDotNetV2;Integrated Security=True ; Encrypt=false");
             //conexion Notebook
             //optionsBuilder.UseSqlServer("Data Source=LAPTOP-UR2EP742\\SQLEXPRESS;Initial Catalog=CineDotNetV2;Integrated Security=True ; Encrypt=false");
+            //conexion Laburo
+            optionsBuilder.UseSqlServer("Data Source=SISTEMAS01\\SQLEXPRESS;Initial Catalog=CineDotNetV2;Integrated Security=True ; Encrypt=false");
 
 
         }
